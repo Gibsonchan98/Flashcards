d46315418b8429b89ad7f17cc46aea102cb60d72
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2023-04-22 12:37:15 -0700

    updated controller

--- API/Controllers/FlashcardController.cs
+++ API/Controllers/FlashcardController.cs
@@ -1,4 +1,5 @@
-using Models; 
+using Services;
+using Models;  
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -8,34 +9,48 @@ namespace API.Controllers;
 public class FlashcardController : ControllerBase
 {
      private readonly ILogger<FlashcardController> _logger;
+     private readonly FService _service;
 
-    public FlashcardController(ILogger<FlashcardController> logger)
+    public FlashcardController(ILogger<FlashcardController> logger, FService service)
     {
-        _logger = logger;
+        this._logger = logger;
+        this._service = service;
     }
 
     [HttpGet(Name = "GetAllFlashcards")]
     public List<Flashcard>? Get(){
-        return null;
+        return this._service.getAllCards();
     }
 
     [HttpGet (Name = "GetFlashcardByID")]
-    public Flashcard? GetByID(int id){
-        return null; 
+    public Flashcard? GetByID([FromQuery] int id){
+        if(id != 0){
+            return this._service.getCardByID(id); 
+        }
+        return null;
     }
 
     [HttpDelete]
-    public bool Delete(int id){
+    public bool Delete([FromQuery] int id){
+        if(id != 0){
+            return this._service.deleteCard(id); 
+        }
         return false;
     }
 
     [HttpPost]
-    public Flashcard? Create(Flashcard cardToBeCreated){
+    public Flashcard? Create([FromBody] Flashcard cardToBeCreated){
+        if(cardToBeCreated != null){
+            return this._service.createCard(cardToBeCreated);
+        }
         return null;
     }
 
     [HttpPut]
-    public Flashcard? Update(Flashcard cardToBeUpdated){
+    public Flashcard? Update([FromBody] Flashcard cardToBeUpdated){
+        if(cardToBeUpdated != null){
+            return this._service.updateCard(cardToBeUpdated);
+        }
         return null;
     }
 }
