c992d0a14e9c01070a337bd0cea6ad94ea88a650
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2023-04-24 12:21:41 -0700

    lets see

--- API/Program.cs
+++ API/Program.cs
@@ -4,7 +4,7 @@ using Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddScoped<IDatabaseRepo, DBRepo>(ctx => new DBRepo(builder.Configuration.GetConnectionString("FlashDB")));
+builder.Services.AddScoped<IDatabaseRepo, DBRepo>(ctx => new DBRepo(builder.Configuration.GetConnectionString("Server=tcp:flashcard.database.windows.net,1433;Initial Catalog=FlashDB;Persist Security Info=False;User ID=CloudSA03a93605;Password=123456F!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")));
 builder.Services.AddScoped<FService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
